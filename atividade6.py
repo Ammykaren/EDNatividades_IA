5286021c1d66df94e27aa3afee1eb74b5a19d1e6
Ammykaren <147573404+Ammykaren@users.noreply.github.com>
2025-05-25 22:24:14 -0300

    feat: add exercicio 6

---
+++ atividade6.py
@@ -0,0 +1,19 @@
+a = 10
+b = 3
+
+print(a + b, a - b, a * b, a / b, a ** b, a // b, a % b)
+
+"""
+Crie quatro novas variaveis de numeros inteiros, some elas, 
+calcule a média e exiba o resultado na tela
+"""
+
+primeiroElemento = 10
+segundoElemento = 20
+terceiroElemento = 30
+quartoElemento = 40
+
+soma = primeiroElemento + segundoElemento + terceiroElemento + quartoElemento
+media = soma / 4
+
+print(f"Média: {media:.2f}")
