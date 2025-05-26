30b427e2fb56b474eac62aab402fc3391d6163b0
Ammykaren <147573404+Ammykaren@users.noreply.github.com>
2025-05-25 22:29:31 -0300

    feat: add exercicio 8

---
+++ atividade8.py
@@ -0,0 +1,16 @@
+
+idade = 18
+
+if idade >= 18:
+    print("Você é um adulto")
+else:
+    print("Você é menor de idade")
+
+#Siga o modelo acima e cria uma nova expressão condicional
+
+comando = "sair"
+
+if comando == "sair":
+    print("você saiu do programa")
+else:
+    print("Você permanece no programa")
