169634749222456ce4d2b9c4d3df9a56fb5d6c1b
Ammykaren <147573404+Ammykaren@users.noreply.github.com>
2025-05-25 22:13:36 -0300

    feat: add exercicio 4

---
+++ atividade4.py
@@ -0,0 +1,14 @@
+"""
+Imprima na tela o tipo de cada variável abaixo, 
+depois mude o tipo de cada uma e verifique o novo tipo
+"""
+
+x = 10
+y = "Dez"
+print(type(x))
+print(type(y))
+
+x = 10.56
+y = True
+print(type(x))
+print(type(y))
